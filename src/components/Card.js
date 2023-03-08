8a4e1ab14040703774d901e7a26a3e250677c4da
draco <122390064+Darco786@users.noreply.github.com>
2023-03-08 16:21:20 +0530

    new update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-
+import heartIcon from "../assets/icons/heart.png";
+import BTCIcon from "../assets/icons/Group.png";
 
 const Card = ({ nftData: { id, img, title, price } }) => {
   return (
-    <section className="font-outfit bg-[#161933]  p-3 rounded-xl shadow-2xl w-auto h-auto flex justify-between gap-4 main-card">
+    <section className="font-outfit bg-[#161933] p-3 rounded-xl shadow-2xl w-auto h-auto flex justify-between gap-4">
       <div>
         <Link
-          to="/"
+          to="/nft-profile"
           className="group block relative rounded-md overflow-hidden"
         >
           <img src={img} alt="equilibrium" className="w-full h-auto" />
@@ -18,7 +18,7 @@ const Card = ({ nftData: { id, img, title, price } }) => {
         <div className="">
           <h1>
             <Link
-              to="/"
+              to="/nft-profile"
               className="block text-white mt-3 font-semibold text-lg hover:text-cyan"
             >
               {title}
@@ -30,17 +30,23 @@ const Card = ({ nftData: { id, img, title, price } }) => {
         </div>
         <div className="flex justify-between items-center">
           <Link
-            to="/"
+            to="/nft-profile"
             className="block text-white  font-semibold text-base hover:text-cyan"
           >
-            Price:{id}
+            Price: {price}
           </Link>
-          <div className="text-[#382C9A] font-bold  text-base  whitespace-nowrap">
-            {price} BTC
+          <div className="flex items-center border border-[#382C9A] p-2 text-white rounded-full font-bold  text-sm  whitespace-nowrap">
+            <img src={BTCIcon} className="h-5 w-5 mr-1" />
+            BTC
           </div>
         </div>
-        <div>Total 7375</div>
-        <div className="flex justify-between items-center mt-5">
+        <Link
+          to="/nft-profile"
+          className="block text-white  font-semibold text-base hover:text-cyan"
+        >
+          Total: {id}
+        </Link>
+        <div className="flex justify-between items-center mt-5 mb-2">
           <div className="space-x-2">
             <a
               href="/Wallet"
@@ -54,9 +60,12 @@ const Card = ({ nftData: { id, img, title, price } }) => {
             >
               Twitter
             </a>
-          </div>
-          <div className="h-9 w-9 p-2 flex justify-center items-center rounded-full bg-[#ffffff0d] cursor-pointer">
-            Discord
+            <a
+              href="/Wallet"
+              className="whitespace-nowrap cursor-pointer rounded-full bg-[#22233B] border-2 border-solid border-transparent py-2 px-4 text-sm text-white hover:text-gray-100"
+            >
+              Discord
+            </a>
           </div>
         </div>
       </div>
